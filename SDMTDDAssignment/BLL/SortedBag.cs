7920fde4e84b12bacc0b42a73f5fb653514ce3a2
LindvedKrvang <rklindved@gmail.com>
2017-09-12 10:30:38 +0200

    created SortedBag class

---
+++ SDMTDDAssignment/BLL/SortedBag.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDMTDDAssignment.BLL
+{
+    public class SortedBag : ISortedBag
+    {
+        public void Add()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Pop()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Count()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
