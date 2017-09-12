de58d3569b2258ca53ec425dbd55d266917b2c48
Adam Hansen <adamlars90@gmail.com>
2017-09-12 10:27:27 +0200

    Added interface

---
+++ SDMTDDAssignment/BLL/ISortedBag.cs
@@ -0,0 +1,14 @@
+﻿namespace SDMTDDAssignment.BLL
+{
+    public interface ISortedBag
+    {
+        // Add integer
+        void Add();
+
+        // Retrieve and remove smallest integer
+        int Pop();
+
+        // Retrieve the number of integers
+        int Count();
+    }
+}
